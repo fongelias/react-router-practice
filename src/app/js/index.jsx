d6de998c067dc521e31ef0ff77a70ff74252e484
fongelias <fongelias@gmail.com>
2018-04-13 21:17:59 -0400

    removed node_modules

--- src/app/js/index.jsx
+++ src/app/js/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+
+const App = () => (
+	<Router>
+		<div>
+			<h1>Hello World</h1>
+			<Link to='/first'>First</Link>
+			<Link to='/second'>Second</Link>
+			<Link to='/third'>Third</Link>
+		</div>
+	</Router>
+);
 
 
 
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
 
 
 
